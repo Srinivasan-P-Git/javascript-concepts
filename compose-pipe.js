fcf2c280e6864f7dd7445c37d616296643740327
SrinuThala <sriniv2s2n@gmail.com>
2022-07-27 21:59:33 +0530

    Update - Compose and Pipe

--- compose-pipe.js
+++ compose-pipe.js
@@ -12,12 +12,40 @@ let compose = (...fns) => (...args) => {
     }, first(...args));
 }
 
-//Example - Compose
+//Example 1 - Compose
 let multiplyByThree = num => num * 3;
 let absoluteNumber = num => Math.abs(num);
 
 let multiplyByThreeAndAbsolute = compose(absoluteNumber, multiplyByThree);
 console.log(multiplyByThreeAndAbsolute(-90));
+
+//Example 2 - Compose
+let user = {
+    name: "Pete",
+    cart: [],
+    purchases: []
+}
+//Add an item to user cart - PURE FUNCTION
+let addItemToCart = (user, item) => {
+    const updatedCart = [...user.cart];
+    updatedCart.push(item);
+    return { ...user, cart: updatedCart };
+}
+//Add tax to items in the user cart - PURE FUNCTION
+let addTax = user => {
+    const updatedCart = user.cart.map(item => {
+        item.price = 1.3 * item.price; //Assume tax to be 30%
+        return item;
+    });
+    return { ...user, cart: updatedCart };
+}
+//Move items in cart to purchases - PURE FUNCTION
+let buyItems = user => ({ ...user, purchases: [...user.cart] });
+//Empty Cart - PURE FUNCTION
+let emptyCart = user => ({ ...user, cart: [] });
+
+let purchaseItem = compose(emptyCart, buyItems, addTax, addItemToCart);
+console.log(purchaseItem(user, { name: "Mobile", price: 15000 }));
 
 /**
  * @function - Pipe
@@ -25,7 +53,7 @@ console.log(multiplyByThreeAndAbsolute(-90));
  * Composition happens from Left to Right
  */
 
- let pipe = (...fns) => (...args) => {
+let pipe = (...fns) => (...args) => {
     const [first, ...remainingFns] = fns;
     return remainingFns.reduce((result, currFn) => {
         return currFn(result);
